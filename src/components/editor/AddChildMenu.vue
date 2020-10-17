b7a60adcc7faebd22348b465fd584c4bd813e86d
Emilio Bondioli <e.bondioli@todo.to.it>
2020-10-17 10:56:05 +0200

    update menu styles

--- src/components/editor/AddChildMenu.vue
+++ src/components/editor/AddChildMenu.vue
@@ -1,8 +1,8 @@
 <template>
   <ul class="add-child-menu">
-    <li @click="$emit('select', 'text')"><i class="ri-text"></i>Text</li>
-    <li @click="$emit('select', 'columns')"><i class="ri-layout-column-line"></i>Columns</li>
-    <li @click="$emit('select', 'group')"><i class="ri-layout-2-line"></i>Group</li>
+    <li @click="$emit('select', 'text')"><i class="ri-text"></i> Text</li>
+    <li @click="$emit('select', 'columns')"><i class="ri-layout-column-line"></i> Columns</li>
+    <li @click="$emit('select', 'group')"><i class="ri-layout-2-line"></i> Group</li>
   </ul>
 </template>
 
@@ -14,9 +14,13 @@ export default {
 
 <style lang="scss" scoped>
 .add-child-menu {
+  font-size: $font-small;
   flex-direction: column;
   li {
     margin-bottom: $padding/2;
+    i {
+      vertical-align: text-top;
+    }
   }
 }
 </style>
