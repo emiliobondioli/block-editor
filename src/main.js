fee35a7eaf83243eecdbbc1a1ebd353f7961e4a3
Emilio Bondioli <e.bondioli@todo.to.it>
2020-10-17 14:23:02 +0200

    install and use vue composition api

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
+import VueCompositionAPI from '@vue/composition-api';
 import 'remixicon/fonts/remixicon.css';
 import './assets/styles/style.scss';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueCompositionAPI);
 
 new Vue({
   store,
